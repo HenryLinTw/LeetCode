fa7b98187e13875f53c230093ba5f736bf28d81c
Heng-Yi Lin <b98611008@ntu.edu.tw>
2016-02-25 16:19:26 -0600

    Update LeetCode3.cc
    update solution comment

--- LeetCode3.cc
+++ LeetCode3.cc
@@ -1,9 +1,20 @@
 //
 //  LeetCode3.cc
-//  
 //  Created by Heng-Yi Lin on 2/25/16.
+//  
+//  title: 3. Longest Substring Without Repeating Characters
+//  Given a string, find the length of the longest substring without repeating characters. For example, the longest 
+//  substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b",
+//  with the length of 1.
 //
-//
+//  solution: 
+//  *use array to present a fake hashtable, store each char with index from its ascii  
+//  loop through the string, 
+//  1) if the char doesn't exist in the substring, increment length and store its index in fake hashtable 
+//  2) if exists, 
+//      a. check if the substring is greater than longest length
+//      b. update start position to next index of the first repeating char and calculate tmp length from that position
+//      c. update fake hashtable for that char
 
 class Solution {
 public:
